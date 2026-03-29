5f7e912d6e7b4c1914bf7fe9e43dbf0c49fe4703
Maria Tapia <mariatm89@yahoo.es>
2026-03-29 12:19:30 +0200

    Fix casing of 'Ana' to 'ana' in insert.sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO form (name, email, subjet, age, message)
 VALUES 
-('Ana', 'ana@gmail.com', 'information', NULL, 'hi testing'),
+('ana', 'ana@gmail.com', 'information', NULL, 'hi testing'),
 (NULL, 'pedro@gmail.com', 'Job', 20, 'pedro message'),
 ('maria','maria@yahoo.com', 'other', 25, 'test message'),
-('Ana', 'ana.work@gmail.com', 'job', 30, 'test message'),
+('ana', 'ana.work@gmail.com', 'job', 30, 'test message'),
 (NULL, 'user1@gmail.com', 'information', 22, 'testing message'),
 ('pedro', 'pedro@test.com', 'other', 35, 'test message large'),
 ('juan', 'juan@gmail.com','job',NULL,'hi qa tested' ),
